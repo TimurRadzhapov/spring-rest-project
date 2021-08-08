7e397c16496149c283ef5e35dedb6646f65d89a7
Timur Radzhapov <timur.radzhapov@sibers.com>
2021-08-08 22:53:14 +0600

    fixes

--- rest-api/src/main/java/com/restapp/restapi/RestApiApplication.java
+++ rest-api/src/main/java/com/restapp/restapi/RestApiApplication.java
@@ -2,10 +2,8 @@ package com.restapp.restapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Profile;
 
 @SpringBootApplication
-@Profile({"local","prod"})
 public class RestApiApplication {
 
     public static void main(String[] args) {
